84ed33963104793fc5df14e454171bad2a725ce2
Nursultan <nursultan@MacBook-Pro-admin.local>
2025-05-30 00:25:28 +0500

    updated logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,26 +4,23 @@ const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-// Функция для записи логов в файл с таймстампом и уникальным ID
+const logsDir = "/tmp/logs"; // Папка для логов в tmp (только для записи в read-only окружениях)
+
 const logEvents = async (message, fileName) => {
   const dateTime = format(new Date(), "yyyyMMdd\tHH:mm:ss");
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
   try {
-    const logsDir = path.join(__dirname, "..", "logs");
-    // Проверяем, существует ли папка logs, если нет - создаём
     if (!fs.existsSync(logsDir)) {
-      await fsPromises.mkdir(logsDir);
+      await fsPromises.mkdir(logsDir, { recursive: true });
     }
 
-    // Добавляем сообщение в файл
     await fsPromises.appendFile(path.join(logsDir, fileName), logItem);
   } catch (err) {
     console.error("Ошибка записи логов:", err);
   }
 };
 
-// Express middleware для логирования каждого HTTP запроса
 const logger = (req, res, next) => {
   logEvents(
     `${req.method}\t${req.url}\t${req.headers.origin || "no-origin"}`,
